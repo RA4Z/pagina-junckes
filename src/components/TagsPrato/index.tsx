3b1f4d02288ccfb2e6617542231d1e0bb92005fb
RA4Z <rraz639@gmail.com>
2023-09-16 14:11:11 -0300

    Organizando as cachaças

--- src/components/TagsPrato/index.tsx
+++ src/components/TagsPrato/index.tsx
@@ -17,8 +17,8 @@ export default function TagsPrato({
             >
                 {category.label}
             </div>
-            <div className={styles.tags__porcao}>{size}g</div>
-            <div className={styles.tags__qtdpessoas}>Serve {serving} pessoa{serving === 1 ? '' : 's'}</div>
+            <div className={styles.tags__porcao}>{size}ml</div>
+            {/* <div className={styles.tags__qtdpessoas}>Serve {serving} pessoa{serving === 1 ? '' : 's'}</div> */}
             <div className={styles.tags__valor}>R$ {price.toFixed(2)}</div>
         </div>
     );
